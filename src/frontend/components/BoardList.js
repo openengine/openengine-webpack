9843f1c4c472ab4910e9c15917b99a1952d5ead6
Luis Escobedo <lescobedo@gmail.com>
2015-10-21 11:54:48 -0500

    Added initial BoardList 'Board Cards' to list view

--- src/frontend/components/BoardList.js
+++ src/frontend/components/BoardList.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import Relay from 'react-relay';
+import Card from 'material-ui/lib/card/card';
+import CardHeader from 'material-ui/lib/card/card-header';
+import CardText from 'material-ui/lib/card/card-text';
+import CardActions from 'material-ui/lib/card/card-actions';
+import Avatar from 'material-ui/lib/avatar'
+import FlatButton from 'material-ui/lib/flat-button'
+import Colors from 'material-ui/lib/styles/colors'
 
 class BoardList extends React.Component {
   renderBoards() {
     const {viewer} = this.props;
 
     return viewer.boards.edges.map(({node}) =>
-      <h2 key={node.id}>{node.title}</h2>
+
+      <Card key={node.id} initiallyExpanded={false}>
+        <CardHeader
+          title={node.title}
+          subtitle= "A Nice Board"
+          avatar={<Avatar color={Colors.amber800} backgroundColor={Colors.green100}>{node.title.charAt(0)}</Avatar>}
+          actAsExpander={true}
+          showExpandableButton={true}>
+        </CardHeader>
+        <CardText expandable={true}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+          Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+          Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+          Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+        </CardText>
+        <CardActions expandable={true}>
+          <FlatButton label="Action1"/>
+          <FlatButton label="Action2"/>
+        </CardActions>
+      </Card>
     );
   }
 
--- src/frontend/components/Navigation.js
+++ src/frontend/components/Navigation.js
@@ -4,6 +4,6 @@ const AppBar = require('material-ui/lib/app-bar');
 
 export default function Navigation(props) {
   return (
-<AppBar title={props.projectName} iconElementLeft={<img src={'img/engine_logo.svg'} />} />
+<AppBar title={props.projectName} iconElementLeft={<IndexLink to="/"><img src={'img/engine_logo.svg'} /></IndexLink>} />
   );
 }
