2ba3d229baf5adee7dcfb5f303066b9a76a6ae40
Dave Bryand <dave@davebryand.com>
2015-10-23 14:52:18 -0700

    Cleanup and styling of app bar

--- src/frontend/components/Navigation.js
+++ src/frontend/components/Navigation.js
@@ -4,6 +4,10 @@ const AppBar = require('material-ui/lib/app-bar');
 
 export default function Navigation(props) {
   return (
-		<AppBar title={<h1><IndexLink className="appBarTitle" to="/">{props.projectName}</IndexLink></h1>} iconElementLeft={<IndexLink to="/"><img src={'img/engine_logo.svg'} /></IndexLink>} />
+    <AppBar
+      title={<h1><IndexLink className="appBarTitle" to="/">{props.projectName}</IndexLink></h1>}
+      iconElementLeft={<IndexLink to="/"><img src={'img/engine_logo.svg'} /></IndexLink>}
+      style={{background: 'none',  'box-shadow': 'none'}}
+    />
   );
 }
