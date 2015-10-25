56d5962d12134d4812a9b01384776653aee46150
Luis Escobedo <lescobedo@gmail.com>
2015-10-25 16:05:33 -0500

    Added Heroku production webpack configuration, and associated gulp tasks.

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,9 +1,14 @@
 
 var path = require('path');
 var webpack = require('webpack');
+var assign = require('object-assign');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
+var defaultConfig = {
+  devtool: 'sourcemap'
+};
+
+var frontendConfig = assign({}, defaultConfig, {
   devtool: 'source-map',
   entry: [
     './src/frontend/index.js'
@@ -48,4 +53,46 @@ module.exports = {
   
     ]
   }
-}
+});
+
+var serverConfig = assign({}, defaultConfig, {
+  entry: './src/server/index.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'server.js',
+    libraryTarget: 'commonjs2'
+  },
+
+  target: 'node',
+  // do not include polyfills or mocks for node stuff
+  node: {
+    console: false,
+    global: false,
+    process: false,
+    Buffer: false,
+    __filename: false,
+    __dirname: false
+  },
+  // all non-relative modules are external
+  // abc -> require('abc')
+  externals: /^[a-z\-0-9]+$/,
+
+  plugins: [
+    // enable source-map-support by installing at the head of every chunk
+    new webpack.BannerPlugin('require("source-map-support").install();',
+      {raw: true, entryOnly: false})
+  ],
+
+  module: {
+    loaders: [
+      {
+        // transpile all .js files using babel
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel?stage=0'
+      }
+    ]
+  }
+});
+
+module.exports = [frontendConfig, serverConfig];
