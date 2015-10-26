78cc83feca891ff24e4dddc20cc545d0d161cbe7
Dave Bryand <dave@davebryand.com>
2015-10-26 12:12:01 -0700

    Updates logo and favicon

--- src/frontend/components/Navigation.js
+++ src/frontend/components/Navigation.js
@@ -5,8 +5,7 @@ const AppBar = require('material-ui/lib/app-bar');
 export default function Navigation(props) {
   return (
     <AppBar
-      title={<h1><IndexLink className="appBarTitle" to="/">{props.projectName}</IndexLink></h1>}
-      iconElementLeft={<IndexLink to="/"><img src={'/img/engine_logo.svg'} /></IndexLink>}
+      iconElementLeft={<IndexLink to="/"><img src={'/img/engine_logo_2.svg'} width="150" /></IndexLink>}
       style={{background: 'none',  boxShadow: 'none'}}
     />
   );
