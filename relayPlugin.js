08ee624257d59a1f577ce55151455a0d47fde815
Dave Bryand <dave@davebryand.com>
2015-12-15 16:50:48 -0800

    Change url for schema.json

--- relayPlugin.js
+++ relayPlugin.js
@@ -5,7 +5,7 @@ import request from 'sync-request';
 // This is a little strange... but we want this http request to be
 // synchronous because it needs to write this json file before it gets
 // read by the babel plugin
-const res = request('GET', 'http://localhost:1337/schema.json');
+const res = request('GET', 'http://localhost:1337/graphql/schema.json');
 fs.writeFileSync('./schema.json', res.getBody());
 
 const schema = require('./schema.json');
